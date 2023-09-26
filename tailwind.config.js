42cd6878c5b2e9eb2fb557b671c74a86ab15b2b6
Jonah Ssegawa <jonahssegawa44532@gmail.com>
2023-09-26 17:37:19 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "index.html",
+    "password.html",
+    "success.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
